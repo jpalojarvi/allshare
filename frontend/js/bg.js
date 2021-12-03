bc6aef008dcdf2b4170da26a6c7133d3957d33e2
Mikael Gustafsson <mikaelgustafsson@Mikaels-MacBook-Pro.local>
2021-12-03 10:26:20 +0200

    toimii jotain

--- frontend/js/bg.js
+++ frontend/js/bg.js
@@ -7,7 +7,6 @@ function myFunction() {
     }
   } 
   function kirjauduFunktio(){
-    console.log('nappia painettu')
     var x = document.getElementById("login-wrapper");
     if (x.style.display === "none") {
       x.style.display = "flex";
@@ -16,7 +15,6 @@ function myFunction() {
     }
   }
   function rekisteroidyFunktio(){
-    console.log('nappia painettu')
     var x = document.getElementById("register-wrapper");
     if (x.style.display === "none") {
       x.style.display = "flex";
@@ -24,11 +22,11 @@ function myFunction() {
       x.style.display = "none";
     }
   }
-  // Get the modal
-  var modal = document.getElementById("id01");
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function (event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  };
+    // Get the modal
+    var modal = document.getElementById("login-wrapper");
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    };
